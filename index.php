0b9ebe0a00028ce2f18844975bd99e89102d8d23
hatemDSH <hatderza@gmail.com>
2018-09-28 20:10:15 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE>
+<html>
+<head>
+   <title> BBC </title> 
+   <meta charset="utf-8"/>
+
+   <style type="text/css">
+
+    #topbar {
+       background-color:#7A0000;
+       width:100%;
+       height:45px;
+    }
+
+   </style>
+</head>
+<body> 
+
+   <div id="container">
+
+      <div id="topbar">
+        <div id="logodiv"> 
+          
+        </div> 
+
+      <div> 
+
+   </div>
+</body> 
+</html>
